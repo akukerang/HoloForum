c63e9fb514e4d1e982fc9564020e5fafc7df8046
akukerang <gabrielsuoth@gmail.com>
2025-09-11 13:55:11 +0700

    Reaction Migration and removed unused rows in threads table

--- database/migrations/2025_09_11_064426_create_reactions_table.php
+++ database/migrations/2025_09_11_064426_create_reactions_table.php
@@ -11,16 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('threads', function (Blueprint $table) {
+        Schema::create('reactions', function (Blueprint $table) {
             $table->id();
-
-            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete(); // Created by
-            $table->foreignId('forum_id')->constrained('forums')->cascadeOnDelete(); // Parent Forum
-            $table->string('title'); 
-            $table->boolean('locked')->default(false);
-
-            // TODO: Tags
             $table->timestamps();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete(); // Who reacted
+            $table->foreignId('post_id')->constrained('posts')->cascadeOnDelete(); // On what post
         });
     }
 
@@ -29,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('threads');
+        Schema::dropIfExists('reactions');
     }
 };
