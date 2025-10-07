c22d10b59e04af3dd83760fce9765ea63b530c58
akukerang <gabrielsuoth@gmail.com>
2025-10-07 19:56:29 +0700

    Logo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -3,10 +3,10 @@ import AppLogoIcon from './app-logo-icon';
 export default function AppLogo() {
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-baseColor text-text">
-                <AppLogoIcon className="size-5 fill-current text-blue" />
+            <div className="flex aspect-square size-8 items-center justify-center rounded-md text-text">
+                <AppLogoIcon />
             </div>
-            <div className="ml-1 grid flex-1 text-left text-sm">
+            <div className="grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">HoloForum</span>
             </div>
         </>
